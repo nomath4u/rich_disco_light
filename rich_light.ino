0a8a67442e731024b3e59433b1d52b4edbcde658
Chris Harper <nomath4u@gmail.com>
2016-02-27 19:10:55 -0800

    Added special functions processing

--- rich_light.ino
+++ rich_light.ino
@@ -5,6 +5,8 @@ int green_pin = D1;
 int red_pin = D2;
 int blue_pin = D0;
 int led_pin = D7;
+bool wheel = false;
+bool blink_red = false;
 
 void setup(){
   pinMode(green_pin, OUTPUT);
@@ -14,15 +16,26 @@ void setup(){
   Particle.function("green", net_green);
   Particle.function("red", net_red);
   Particle.function("blue", net_blue);
+  Particle.function("colors", net_set_colors);
+  Particle.function("special", net_special);
 }
 
 void loop(){
   int i = 0;
-  for(i=0; i < ANALOG_MAX; i++){ //Conveniently using analog_max because it is the same max number
-    //Wheel(i);
-    delay(10);
+  if(wheel){
+    for(i=0; i < ANALOG_MAX; i++){ //Conveniently using analog_max because it is the same max number
+        Wheel(i);
+        delay(10);
+    }
   }
-  digitalWrite(led_pin, HIGH);
+  if(blink_red){
+      clear_colors();
+      set_red(255);
+      delay(100);
+      set_red(0);
+      delay(1000);
+  }
+  //digitalWrite(led_pin, HIGH);
 }
 
 void Wheel(byte WheelPos) {
@@ -38,6 +51,11 @@ void Wheel(byte WheelPos) {
   }
 }
 
+void clear_colors(){
+    set_red(0);
+    set_green(0);
+    set_blue(0);
+}
 void color(uint8_t red, uint8_t green, uint8_t blue){
   set_red(red);
   set_green(green);
@@ -66,4 +84,33 @@ int net_red(String val){
 
 int net_blue(String val){
     set_blue(val.toInt());
+}
+
+/*This function expects a comma separated string in the form of "red,green,blue" */
+int net_set_colors(String vals){
+    int commaIndex = vals.indexOf(',');
+    int secondCommaIndex = vals.indexOf(',', commaIndex+1);
+    String red_str = vals.substring(0, commaIndex);
+    String green_str = vals.substring(commaIndex+1, secondCommaIndex);
+    String blue_str = vals.substring(secondCommaIndex+1); 
+    set_red(red_str.toInt());
+    set_green(green_str.toInt());
+    set_blue(blue_str.toInt());
+    turn_off_specials();
+    return blue_str.toInt();
+}
+
+void turn_off_specials(){
+    wheel = false;
+    blink_red = false;
+}
+
+int net_special(String command){
+    turn_off_specials();
+    if(command == "Wheel"){
+        wheel = true;
+    }
+    if(command == "Blink Red"){
+        blink_red = true;
+    }
 }
